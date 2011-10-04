734cde8c7a5a88e686a8638550116434d1c61c21
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2011-10-03 23:48:47 -0500

    Added fruit model

--- Poseidon/Poseidon/PlayGameScene.cs
+++ Poseidon/Poseidon/PlayGameScene.cs
@@ -541,7 +541,7 @@ namespace Poseidon
                             int powerType = random.Next(3) + 1;
                             Fruit fruit = new Fruit(powerType);
                             fruits.Add(fruit);
-                            fruit.LoadContent(Content, "Models/fuelcell", plant.Position);
+                            fruit.LoadContent(Content, "Models/fruit", plant.Position);
                             plant.timeForFruit = false;
                             plant.fruitCreated++;
                         }

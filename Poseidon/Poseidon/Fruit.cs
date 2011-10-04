c71696543d29e4fb20f553cc70a0f0b9f9051bfc
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2011-10-04 11:29:43 -0500

    REmoved the fuelcell class

--- Poseidon/Poseidon/Fruit.cs
+++ Poseidon/Poseidon/Fruit.cs
@@ -5,19 +5,30 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+//using Microsoft.Xna.Framework.Audio;
 
 namespace Poseidon
 {
-    public class Fruit : FuelCell
+    public class Fruit : GameObject
     {
+        public bool Retrieved { get; set; }
+        //SoundEffect RetrievedSound;
+        //Temporary power-up types
+        //1: speed
+        //2: power
+        //3: fire rate
+        public int powerType;
+
         public Fruit(int powerType)
-            : base(powerType)
+            : base()
         {
+            Retrieved = false;
+            this.powerType = powerType;
         }
 
         public void LoadContent(ContentManager content, Vector3 plantPosition)
         {
-            if(this.powerType ==1 )
+            if (this.powerType == 1)
                 Model = content.Load<Model>("Models/green-fruit");
             else if (this.powerType == 2)
                 Model = content.Load<Model>("Models/red-fruit");
@@ -34,5 +45,58 @@ namespace Poseidon
             BoundingSphere =
                 new BoundingSphere(scaledSphere.Center, scaledSphere.Radius);
         }
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+            Matrix[] transforms = new Matrix[Model.Bones.Count];
+            Model.CopyAbsoluteBoneTransformsTo(transforms);
+            Matrix translateMatrix = Matrix.CreateTranslation(Position);
+            Matrix worldMatrix = translateMatrix;
+
+            foreach (ModelMesh mesh in Model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World =
+                        worldMatrix * transforms[mesh.ParentBone.Index];
+                    effect.View = view;
+                    effect.Projection = projection;
+
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+                    //if (powerType == 1)
+                    //{
+                    //    effect.DiffuseColor = Color.Gold.ToVector3();
+                    //}
+                    //else if (powerType == 2)
+                    //{
+                    //    effect.DiffuseColor = Color.Red.ToVector3();
+                    //}
+                    //else if (powerType == 3)
+                    //{
+                    //    effect.DiffuseColor = Color.Blue.ToVector3();
+                    //}
+                }
+                mesh.Draw();
+            }
+        }
+
+        internal void Update(KeyboardState keyboardState, BoundingSphere vehicleBoundingSphere, BoundingSphere vehicleTrashFruitBoundingSphere)
+        {
+            //if (vehicleBoundingSphere.Intersects(this.BoundingSphere))
+            //    this.Retrieved = true;
+            //if (keyboardState.IsKeyDown(Keys.Z))
+            //{
+            //    if (vehicleTrashFruitBoundingSphere.Intersects(this.BoundingSphere))
+            //    {
+            //        RetrievedSound.Play();
+            //        this.Retrieved = true;
+            //    }
+                
+            //}
+            
+        }
+
     }
+
 }

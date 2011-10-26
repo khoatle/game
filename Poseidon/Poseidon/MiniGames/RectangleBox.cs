aebe3507fc8728447d364bdfa594562c0e23b85a
unknown <Thunder-GoD@.(none)>
2011-10-25 23:04:18 -0500

    merging

---
+++ Poseidon/Poseidon/MiniGames/RectangleBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Poseidon.MiniGames
+{
+    class RectangleBox {
+        private Rectangle box;
+        protected int posX;
+        protected int posY;
+        protected int width;
+        protected int height;
+        private Texture2D background;
+
+        public RectangleBox(int X, int Y, int recWidth, int recHeight) {
+            posX = X;
+            posY = Y;
+            width = recWidth;
+            height = recHeight;
+
+            box = new Rectangle(posX, posY, width, height);
+        }
+
+        public virtual void initialize() { 
+            
+        }
+
+        public virtual void loadContent(Texture2D background) {
+            this.background = background;
+
+        }
+
+        public virtual void update(GameTime gameTime) { 
+            
+        }
+
+        public virtual void draw(SpriteBatch spriteBatch) {
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(background, box, Color.White);
+
+            spriteBatch.End();
+        }
+    }
+}
---
+++ Poseidon/Poseidon/MiniGames/Textbox.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Poseidon.MiniGames
+{
+    class Textbox : RectangleBox {
+        private int markupIndex;
+        private List<string> words;
+        private SpriteFont font;
+
+        public Textbox(int X, int Y, int recWidth, int recHeight, string text) 
+            : base(X, Y, recWidth, recHeight) {
+                markupIndex = 0;
+                parseText(text);
+        }
+
+        public void loadContent(Texture2D background, SpriteFont font) {
+            loadContent(background);
+            this.font = font;
+            doWordWrapping();
+        }
+
+        public override void draw(SpriteBatch spriteBatch)
+        {
+            base.draw(spriteBatch);
+            spriteBatch.Begin();
+
+            Vector2 nextDrawPosition = new Vector2(posX, posY);
+
+            for (int i = 0; i < words.Count; i++) {
+                if (words[i].Equals("\n")) {
+                    nextDrawPosition.X = posX;
+                    nextDrawPosition.Y += font.LineSpacing;
+                }
+                else if (i == markupIndex) {
+                    spriteBatch.DrawString(font, words[i] + " ", nextDrawPosition, Color.Yellow);
+                    nextDrawPosition.X += font.MeasureString(words[markupIndex] + " ").X;
+                } else {
+                    spriteBatch.DrawString(font, words[i] + " ", nextDrawPosition, Color.White);
+                    nextDrawPosition.X += font.MeasureString(words[i] + " ").X;
+                }
+            }
+            spriteBatch.End();
+        }
+
+        public void incrementMarkUpIndex() {
+            markupIndex++ ;
+        }
+
+        public int getMarkupIndex() {
+            return markupIndex;
+        }
+
+        private void doWordWrapping()
+        {
+            string line = "";
+            for (int i = 0; i < words.Count; i++) {
+                if (font.MeasureString(line + words[i]).X > width) {
+                    if (markupIndex == i) {
+                        markupIndex++;
+                    }
+                    words.Insert(i, "\n");
+                    line = "";
+                }
+                line += words[i] + " ";
+            }
+        }
+
+        private void parseText(string text)
+        {
+            string[] array = text.Split(' ');
+            words = new List<string>();
+
+            for (int i = 0; i < array.Length; i++) {
+                words.Add(array[i]);
+            }
+        }
+
+        public List<string> getWords() {
+            return words;
+        }
+    }
+}

2aa91693b4f797923b749edd247ace7f40467b0a
Debjyoti Majumder <debjyoti.majumder@gmail.com>
2011-11-08 02:50:50 -0600

    Added more questions and pargraphs for minigames

--- Poseidon/Poseidon/MiniGames/ParagraphLibrary.cs
+++ Poseidon/Poseidon/MiniGames/ParagraphLibrary.cs
@@ -51,6 +51,44 @@ namespace Poseidon.MiniGames
             paragraph = new Paragraph("Scientists say that global warming, by increasing sea water temperatures, will raise sea levels and change ocean currents. The effects are already beginning to be felt. Whole species of marine animals and fish are at risk due to the temperature rise.");
             paragraphLib.Add(paragraph);
 
+            paragraph = new Paragraph("Fifty-five million years ago, ocean acidification led to a mass extinction of ocean creatures. According to our fossil record, it took more than 100,000 years for the oceans to recover.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("'Save Earth' is an incorrect phrase. Humans couldn't begin to threaten a planet. What we threaten is our own civilization, and perhaps our species, plus a few million others. That's just a short-term setback in the life of a planet.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("Our planet is four and half billion years old. There has been life on this planet for nearly that long. Three point eight billion years. The first bacteria. And, later, the first multicellular animals, then the first complex creatures, in the sea, on the land.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("Although the heyday of whaling ended long ago, whaling still occurs, as scientific research, aboriginal cultural whaling, and as outright commercial hunting. Learn more about regulation of whaling and who's hunting whales today.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("Centuries of fishing have caused profound effects on fish populations. A big field in marine conservation is assessing fish populations and restoring depleted populations. Learn about overfishing and issues related to fishing.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("It's still probably fair to say that the greatest threat from climate change faces small island nations that could be washed underwater with just a slight rise in sea levels. Maldives, Bangladesh and Phillipines are at high risk.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("The water is already rising in the Philippines, not only threatening homes of people who live near the coast, but flooding rice fields and devastating other areas of agricultural production.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("Climate change is not an issue of economic development; it's an issue of human survival. Some of Kiribati's 94,000 people living in shoreline village communities have already been relocated from century-old sites.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("Vietnam has already experienced rising sea levels, record temperatures, and unstable rainfall causing unpredictable phases of flood and drought. The rice bowl of Vietnam will be in acute danger.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("The term 'mangrove' refers to a habitat comprised of salt-tolerant plants. Mangrove plants have a tangle of roots which are often exposed above water, leading to the nickname 'walking trees'. Their their leaves can excrete salt.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("While the majority of coral reefs are found in tropical and sub-tropical water within the latitudes of 30 degrees north and 30 degrees south, there are also deep water corals in colder regions.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("The deep sea includes the deepest, darkest, coldest parts of the ocean. Eighty percent of the ocean consists of waters greater than 1,000 meters in depth. A surprisingly large number of species thrive in this environment.");
+            paragraphLib.Add(paragraph);
+
+            paragraph = new Paragraph("When carbon dioxide dissolves in the ocean, carbonic acid is formed. This leads to higher acidity mainly near the surface, which has been proven to inhibit shell growth in marine animals.");
+            paragraphLib.Add(paragraph);
         }
     }
 }

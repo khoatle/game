8ea1e550a2998b47a726a7ba86182b6452813509
unknown <Admin@.(none)>
2012-01-19 01:40:53 +0700

    trying to merge

---
+++ Poseidon/Poseidon/Miscellaneous/Factory.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Poseidon
+{
+    public class Factory : GameObject
+    {
+        public FactoryType factoryType;
+
+        public float orientation;
+
+        public float fogEndValue = GameConstants.FogEnd;
+        public float fogEndMaxVal = 1000.0f;
+        public bool increaseFog = true;
+
+        public Factory(FactoryType factorytype)
+            : base()
+        {
+            this.factoryType = factorytype;
+        }
+
+        public void LoadContent(ContentManager content, string modelname, Vector3 position, float orientation)
+        {
+            Model = content.Load<Model>(modelname);
+            Position = position;
+            BoundingSphere = CalculateBoundingSphere();
+
+            BoundingSphere scaledSphere;
+            scaledSphere = BoundingSphere;
+            scaledSphere.Radius *= GameConstants.FactoryBoundingSphereFactor;
+            BoundingSphere =
+                new BoundingSphere(scaledSphere.Center, scaledSphere.Radius);
+            Vector3 tempCenter;
+            tempCenter = BoundingSphere.Center;
+            tempCenter.X = Position.X;
+            tempCenter.Y = GameConstants.MainGameFloatHeight;
+            tempCenter.Z = Position.Z;
+            BoundingSphere = new BoundingSphere(tempCenter,BoundingSphere.Radius);
+
+            this.orientation = orientation;
+            //if (orientation > 50) floatUp = true;
+            //else floatUp = false;
+        }
+        public void Update(GameTime gameTime)
+        {
+            if (increaseFog)
+                fogEndValue += 2.5f;
+            else fogEndValue -= 2.5f;
+            if (fogEndValue > fogEndMaxVal || fogEndValue < GameConstants.FogEnd)
+                increaseFog = !increaseFog;
+        }
+        public void Draw(Matrix view, Matrix projection)
+        {
+            Matrix[] transforms = new Matrix[Model.Bones.Count];
+            Model.CopyAbsoluteBoneTransformsTo(transforms);
+            Matrix translateMatrix = Matrix.CreateTranslation(Position);
+            Matrix rotationYMatrix = Matrix.CreateRotationY(orientation);
+            Matrix worldMatrix = rotationYMatrix * translateMatrix;
+
+            foreach (ModelMesh mesh in Model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World =
+                        worldMatrix * transforms[mesh.ParentBone.Index];
+                    effect.View = view;
+                    effect.Projection = projection;
+
+                    effect.EnableDefaultLighting();
+                    effect.PreferPerPixelLighting = true;
+
+                    //effect.DiffuseColor = Color.Green.ToVector3();
+
+                    effect.FogEnabled = true;
+                    effect.FogStart = GameConstants.FogStart;
+                    effect.FogEnd = fogEndValue;// GameConstants.FogEnd;
+                    effect.FogColor = GameConstants.FogColor.ToVector3();
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
